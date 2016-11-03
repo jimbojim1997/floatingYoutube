ccc2ff538c2d98c585455bf384242d9450a60d10
jimbojim1997 <jimbojim1997@gmail.com>
2016-11-03 22:49:26 +0000

    Fixed issue where the player would be scaled to 0. Also made CalculateScale function generic

--- src/main.js
+++ src/main.js
@@ -1,27 +1,28 @@
-var player;
-var sidebar;
-var isFloating = false;
-var originalPlayerStyle;
 var scrollTopThreshold = 10;
 var playerFlaotTop = 50;
 
-Load();
+var isFloating = false;
+var playerID = "player-api";
+var sidebarID = "watch7-sidebar-contents";
+
+window.addEventListener("load", Load, false);
 
 function Load() {
-    player = document.getElementById("player-api");
-    sidebar = document.getElementById("watch7-sidebar-contents");
-    originalPlayerStyle = JSON.parse(JSON.stringify(player.style)); //clone player style to later revert to
+    //originalPlayerStyle = JSON.parse(JSON.stringify(player.style)); //clone player style to later revert to
 
     window.addEventListener("scroll", PageScroll, false);
     window.addEventListener("resize", PageResize, false);
-    console.log("<Floating Youtube>: Loaded");
-
     PageScroll();
+	
+	Log("Loaded");
 }
 
 function PageScroll(e) {
     if (window.pageYOffset > scrollTopThreshold && !isFloating) {//make video float
-        var playerScaleFactor = CalculatePlayerScale();
+        var player = _(playerID);
+		var sidebar = _(sidebarID);
+		
+		var playerScaleFactor = CalculateScale(player, sidebar);
 
         player.style.position = "fixed";
         player.style.top = playerFlaotTop + "px";
@@ -30,31 +31,53 @@ function PageScroll(e) {
         player.style.transform = "scale(" + playerScaleFactor + ")";
 
         isFloating = true;
-        console.log("<Floating Youtube>: Player floating:true");
+        Log("Player floating:true");
 
     } else if (window.pageYOffset < scrollTopThreshold && isFloating) {//reset video
-        player.style = originalPlayerStyle;
+		var player = _(playerID);
+		var sidebar = _(sidebarID);
+		
+        player.style.position = "absolute";
+        player.style.top = "0px";
+        player.style.left = "0px";
+        player.style.transformOrigin = "top left";
+        player.style.transform = "scale(1)";
 
         isFloating = false;
-        console.log("<Floating Youtube>: Player floating:false");
+        Log("Player floating:false");
     }
 }
 
 function PageResize(e) {
     if (isFloating) {
-        var playerScaleFactor = CalculatePlayerScale();
+		var player = _(playerID);
+		var sidebar = _(sidebarID);
+		
+        var playerScaleFactor = CalculatePlayerScale(player, sidebar);
         player.style.left = sidebar.getBoundingClientRect().left + "px";
         player.style.transform = "scale(" + playerScaleFactor + ")";
     }
 }
 
-function CalculatePlayerScale() {
-    var left = sidebar.clientLeft;
-    var currentWidth = player.clientWidth;
-    
-    var availableWidth = sidebar.clientWidth;
+function CalculateScale(target, scaleTo) {
+    var currentWidth = target.clientWidth;    
+    var availableWidth = scaleTo.clientWidth;
 
     var scale = availableWidth / currentWidth;
-
+	
     return scale;
+}
+
+function Log(message){
+	if(typeof(message) == typeof("")){
+		console.log("<Floating Youtube>: " + message);	
+	}else{
+		console.group("<Floating Youtube>:");
+		console.log(message);
+		console.groupEnd();
+	}
+}
+
+function _(id){
+	return document.getElementById(id);
 }
